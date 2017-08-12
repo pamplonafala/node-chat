fd90a141cd72d6d8a998ff8ef2db126562840135
pamplonafala <pamplonafala@gmail.com>
2017-08-12 06:04:42 -0300

    Identing

--- server.js
+++ server.js
@@ -10,23 +10,23 @@ for (var a in ifaces) {
 	for (var b in ifaces[a]) {
 	    var addr = ifaces[a][b];
 	    if (addr.family === 'IPv4' && !addr.internal) {
-		console.log("Network IP: " + addr.address );
+			console.log("Network IP: " + addr.address );
 	    }
 	}
 }
 
 var server = http.createServer(function(req, resp) {
     if (req.url == "/new.html") {
-      var read = fs.createReadStream(__dirname + req.url);
-      read.pipe(resp);
-      resp.writeHead(200, {"Content-Type": "text/html"});
+		var read = fs.createReadStream(__dirname + req.url);
+		read.pipe(resp);
+		resp.writeHead(200, {"Content-Type": "text/html"});
     } else if (req.url == "/style.css" || req.url == "/client.js") {
-      resp.writeHead(200, {"Content-Type":  
+		resp.writeHead(200, {"Content-Type":  
 		req.url == '/style.css' ? 'text/css' : 'application/javascript'}); 
-      fs.createReadStream(__dirname + req.url).pipe(resp);
+		fs.createReadStream(__dirname + req.url).pipe(resp);
     } else {
-	resp.writeHead(400, "Invalid URL/Method"); 
-	resp.end();
+		resp.writeHead(400, "Invalid URL/Method"); 
+		resp.end();
     }
 });
 
